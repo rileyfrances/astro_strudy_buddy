fd13daf215d6d0dcb1b87b223ae2e1ba78dad432
rileyfrances <11095050@cc.peralta.edu>
2026-05-18 05:34:18 +0000

    Remove build files from repository

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/astro_strudy_buddy
-# Build directory: /workspaces/astro_strudy_buddy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GameTests "/workspaces/astro_strudy_buddy/build/tests")
-set_tests_properties(GameTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/astro_strudy_buddy/CMakeLists.txt;20;add_test;/workspaces/astro_strudy_buddy/CMakeLists.txt;0;")
